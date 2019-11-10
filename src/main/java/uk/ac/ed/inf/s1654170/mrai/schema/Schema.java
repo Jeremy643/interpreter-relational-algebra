cfb2199f5aa7ec009e9b4733b3977d01338462a8
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-10 19:09:36 +0000

    Uncomment Schema constructor

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/Schema.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/Schema.java
@@ -1,5 +1,6 @@
 package uk.ac.ed.inf.s1654170.mrai.schema;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -7,9 +8,9 @@ public class Schema {
 	
 	private Map<String,Signature> relations;
 	
-//	public Schema(Map<String,HashMap<String,Type>> relations) {
-//		this.relations = new HashMap<>(relations);
-//	}
+	public Schema(Map<String,Signature> relations) {
+		this.relations = new HashMap<>(relations);
+	}
 	
 	public Set<String> getRelations() {
 		return relations.keySet();
