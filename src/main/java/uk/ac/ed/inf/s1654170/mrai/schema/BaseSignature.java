f506cf59a9a4530cfec619d7c94b5b99cd35c8b2
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-11 23:49:02 +0000

    Make instance variables private and fix constructor

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/BaseSignature.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/BaseSignature.java
@@ -7,15 +7,15 @@ import uk.ac.ed.inf.s1654170.mrai.schema.Column.Type;
 
 public class BaseSignature implements Signature {
 	
-	List<String> names;
-	List<Type> types;
+	private List<String> names;
+	private List<Type> types;
 
 	public BaseSignature(List<String> attr, List<Type> types) {
 		if (attr.size() != types.size()) {
 			throw new RuntimeException();
 		}
 		names = new ArrayList<String>(attr);
-		types = new ArrayList<Type>(types);
+		this.types = new ArrayList<Type>(types);
 	}
 	
 	@Override
