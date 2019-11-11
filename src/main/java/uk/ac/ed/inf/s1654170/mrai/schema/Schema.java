6d60e35489c14780bcb47de623f33b53f2970996
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-11 17:42:27 +0000

    Implement Schema constructor to handle string input

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/Schema.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/Schema.java
@@ -1,15 +1,48 @@
 package uk.ac.ed.inf.s1654170.mrai.schema;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import uk.ac.ed.inf.s1654170.mrai.schema.Column.Type;
 
 public class Schema {
 	
 	private Map<String,Signature> relations;
 	
-	public Schema(Map<String,Signature> relations) {
-		this.relations = new HashMap<>(relations);
+	public Schema(String relations) { //Map<String,Signature> relations
+		//this.relations = new HashMap<>(relations);
+		this.relations = new HashMap<>(formatInput(relations));
+	}
+	
+	private HashMap<String,Signature> formatInput(String rel) {
+		HashMap<String,Signature> returnRelations = new HashMap<>();
+		
+		//remove any whitespace that might be in the input
+		rel = rel.replaceAll(" ", "");
+		
+		String[] relations = rel.split(";");
+		for (String relation : relations) {
+			List<String> attr = new ArrayList<>();
+			List<Type> type = new ArrayList<>();
+			
+			String[] relAttributes = relation.split(":");
+			String relName = relAttributes[0];
+			
+			String[] attributes = relAttributes[1].split(",");
+			for (String attribute : attributes) {
+				String[] attributeType = attribute.split("/");
+				attr.add(attributeType[0]);
+				type.add(Type.valueOf(attributeType[1]));
+			}
+			
+			Signature sig = new BaseSignature(attr, type);
+			returnRelations.put(relName, sig);
+		}
+		
+		return returnRelations;
 	}
 	
 	public Set<String> getRelations() {
