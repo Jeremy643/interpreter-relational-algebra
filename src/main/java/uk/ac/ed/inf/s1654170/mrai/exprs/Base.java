c792059c2d0b9c690a87326c5513f9cae51f7b46
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-14 14:47:54 +0000

    Implement validate method

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/exprs/Base.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/exprs/Base.java
@@ -1,6 +1,10 @@
 package uk.ac.ed.inf.s1654170.mrai.exprs;
 
+import uk.ac.ed.inf.s1654170.mrai.schema.Schema;
+import uk.ac.ed.inf.s1654170.mrai.schema.Signature;
+
 public class Base extends RAExpr {
+	
 	private String name;
 	
 	public Base(String name) {
@@ -15,5 +19,20 @@ public class Base extends RAExpr {
 	@Override
 	public String toString() {
 		return name;
+	}
+
+//	@Override
+//	public Signature getSignature() {
+//		return null;
+//	}
+
+	@Override
+	public boolean validate(Schema schema) {
+		Signature sig = schema.getSignature(name);
+		if (sig == null) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
