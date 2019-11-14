49475b80bed962de6d6484b1d3a9cf2e73fa9e0d
JeremyScott17 <s1654170@sms.ed.ac.uk>
2019-11-14 14:48:40 +0000

    Comment out getSignature method

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/exprs/RAExpr.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/exprs/RAExpr.java
@@ -37,7 +37,7 @@ public abstract class RAExpr {
 		return type;
 	}
 	
-	public abstract Signature getSignature();
+	//public abstract Signature getSignature();
 	
 	public abstract boolean validate(Schema schema);
 }
