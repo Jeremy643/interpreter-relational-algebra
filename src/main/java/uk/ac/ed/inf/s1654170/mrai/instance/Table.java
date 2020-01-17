48d0da0a5311f0d5f19cc5b7d81bf67904f085bf
JeremyScott17 <s1654170@sms.ed.ac.uk>
2020-01-17 18:38:47 +0000

    Implement Table class to have its signature

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/instance/Table.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/instance/Table.java
@@ -2,10 +2,26 @@ package uk.ac.ed.inf.s1654170.mrai.instance;
 
 import java.util.ArrayList;
 
+import uk.ac.ed.inf.s1654170.mrai.schema.Signature;
+
 public class Table extends ArrayList<Record> {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2180789515268198297L;
+	
+	private Signature signature;
+	
+	public Table(Signature signature) {
+		this.signature = signature;
+	}
+	
+	public Signature getSignature() {
+		return signature;
+	}
+	
+	public int getAttributePosition(String attribute) {
+		return signature.getAttributes().indexOf(attribute);
+	}
 }
