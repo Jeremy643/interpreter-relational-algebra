d07af8158212aca05e508e9edbcb19ec263ae911
JeremyScott17 <s1654170@sms.ed.ac.uk>
2020-02-15 12:53:47 +0000

    Update schema error given for renaming

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/SchemaException.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/SchemaException.java
@@ -7,7 +7,7 @@ public class SchemaException extends Exception {
 	public static final String DIFFERENCE_ERROR = "DIFFERENCE ERROR: The arguments do not have the same set of attributes or types.";
 	public static final String INTERSECT_ERROR = "INTERSECT ERROR: The arguments do not have the same set of attributes or types.";
 	public static final String PROJECT_ERROR = "PROJECT ERROR: The attributes to be projected over do not appear in the relation.";
-	public static final String RENAME_ERROR = "RENAME ERROR: Not all of the attributes to be renamed appear in the relation.";
+	public static final String RENAME_ERROR = "RENAME ERROR: You have a mistake in the renaming of one or more of your attributes.";
 	public static final String SELECT_ERROR = "SELECT ERROR: The condition is not valid.";
 	
 	public static final String NO_SAME_ATTR = "The input arguments must have the same attributes and types";
