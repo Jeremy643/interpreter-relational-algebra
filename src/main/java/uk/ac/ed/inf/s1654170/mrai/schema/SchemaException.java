3693f485d39d608829d5172c402c4427caf5b061
JeremyScott17 <s1654170@sms.ed.ac.uk>
2020-02-22 23:00:52 +0000

    Add a new error message to be thrown for an unordered signature

--- src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/SchemaException.java
+++ src/main/java/uk/ac/ed/inf/s1654170/mrai/schema/SchemaException.java
@@ -10,6 +10,8 @@ public class SchemaException extends Exception {
 	public static final String RENAME_ERROR = "RENAME ERROR: You have a mistake in the renaming of one or more of your attributes.";
 	public static final String SELECT_ERROR = "SELECT ERROR: The condition is not valid.";
 	
+	public static final String UNORDERED_SIGNATURE_ERROR = "There are duplicate attributes.";
+	
 	public static final String NO_SAME_ATTR = "The input arguments must have the same attributes and types";
 	
 	public static final String INSERT_ERROR = "INSERT ERROR: You are trying to insert data into a relation that does not exist in the schema.";
