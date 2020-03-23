0240d26835478567e24a2f1c5c161cc3e9af8d26
JeremyScott17 <s1654170@sms.ed.ac.uk>
2020-03-23 16:58:13 +0000

    Implement tests for the new approach
    * Implement tests that cover all of the operations for a basic
      expression when the columns are ordered

--- src/test/java/uk/ac/ed/inf/s1654170/mrai/evaluation/ordered/OrderedTest.java
+++ src/test/java/uk/ac/ed/inf/s1654170/mrai/evaluation/ordered/OrderedTest.java
@@ -19,9 +19,14 @@ import org.junit.jupiter.api.Test;
 
 import uk.ac.ed.inf.s1654170.mrai.evaluation.GetDataHelper;
 import uk.ac.ed.inf.s1654170.mrai.exprs.Base;
+import uk.ac.ed.inf.s1654170.mrai.exprs.Difference;
 import uk.ac.ed.inf.s1654170.mrai.exprs.Eliminate;
+import uk.ac.ed.inf.s1654170.mrai.exprs.Intersect;
+import uk.ac.ed.inf.s1654170.mrai.exprs.Product;
+import uk.ac.ed.inf.s1654170.mrai.exprs.Project;
 import uk.ac.ed.inf.s1654170.mrai.exprs.RAExpr;
 import uk.ac.ed.inf.s1654170.mrai.exprs.Union;
+import uk.ac.ed.inf.s1654170.mrai.exprs.UnionMax;
 import uk.ac.ed.inf.s1654170.mrai.instance.Record;
 import uk.ac.ed.inf.s1654170.mrai.instance.Table;
 import uk.ac.ed.inf.s1654170.mrai.schema.BaseSignature;
@@ -115,27 +120,21 @@ class OrderedTest {
 		assertEquals(expected, e.execute(dbSets));
 	}
 	
-//	@Test
-//	void testEliminateBags() throws SchemaException {
-//		// <E>(SportStudents)
-//		RAExpr e = new Eliminate(new Base("SportStudents"));
-//		Signature sig = dbBags.getSchema().getSignature("SportStudents");
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(sig.getTypes(), "Hamish", "18", "s6"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Sean", "14", "s3"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//	
-//	@Test
-//	void testEliminateSets() throws SchemaException {
-//		// <E>(Parents)
-//		RAExpr e = new Eliminate(new Base("Parents"));
-//		Signature sig = dbSets.getSchema().getSignature("Parents");
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(sig.getTypes(), "Hugo", "3/09/88", "2"));
-//		assertEquals(expected, e.execute(dbSets));
-//	}
+	@Test
+	void testEliminateBags() throws SchemaException, IOException {
+		// <E>(SportStudents)
+		RAExpr e = new Eliminate(new Base("SportStudents"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+	
+	@Test
+	void testEliminateSets() throws SchemaException, IOException {
+		// <E>(Parents)
+		RAExpr e = new Eliminate(new Base("Parents"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbSets));
+	}
 
 	@Test
 	void testUnionBags() throws SchemaException, IOException {
@@ -146,32 +145,21 @@ class OrderedTest {
 	}
 
 //	@Test
-//	void testUnionSets() throws SchemaException {
-//		// Students <+> SportStudents
-//		RAExpr e = new Union(new Base("Students"), new Base("SportStudents"));
-//		Signature studentSig = dbSets.getSchema().getSignature("Students");
-//		Table expected = new Table(studentSig);
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Hamish", "18", "s6"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Jane", "18", "s6"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Sean", "14", "s3"));
+//	void testUnionSets() throws SchemaException, IOException {
+//		// Students <+> DetentionStudents
+//		RAExpr e = new Union(new Base("Students"), new Base("DetentionStudents"));
+//		Table expected = getExpectedTable(e.toString());
 //		assertEquals(expected, e.execute(dbSets));
 //	}
-//
-//	@Test
-//	void testUnionMaxBags() throws SchemaException {
-//		// Students <U> SportStudents
-//		RAExpr e = new UnionMax(new Base("Students"), new Base("SportStudents"));
-//		Signature studentSig = dbBags.getSchema().getSignature("Students");
-//		Table expected = new Table(studentSig);
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Hamish", "18", "s6"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Jane", "18", "s6"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(studentSig.getTypes(), "Sean", "14", "s3"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//
+
+	@Test
+	void testUnionMaxBags() throws SchemaException, IOException {
+		// Students <U> SportStudents
+		RAExpr e = new UnionMax(new Base("Students"), new Base("SportStudents"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+
 //	@Test
 //	void testUnionMaxSets() throws SchemaException {
 //		// Students <U> SportStudents
@@ -184,24 +172,18 @@ class OrderedTest {
 //		expected.add(Record.valueOf(studentSig.getTypes(), "Sean", "14", "s3"));
 //		assertEquals(expected, e.execute(dbSets));
 //	}
-//
-//	@Test
-//	void testProjectBags() throws SchemaException {
-//		List<String> attr = new ArrayList<>();
-//		attr.add("Name");
-//		attr.add("Teach");
-//		List<Column.Type> types = new ArrayList<>();
-//		types.add(Column.Type.STRING);
-//		types.add(Column.Type.STRING);
-//		// <P>[Name,Teach](Teachers)
-//		RAExpr e = new Project(new Base("Teachers"), attr);
-//		Signature sig = new BaseSignature(attr, types, ordered);
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(types, "David", "s3"));
-//		expected.add(Record.valueOf(types, "Tom", "s1"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//
+
+	@Test
+	void testProjectBags() throws SchemaException, IOException {
+		List<String> attr = new ArrayList<>();
+		attr.add("Name");
+		attr.add("Teach");
+		// <P>[Name,Teach](Teachers)
+		RAExpr e = new Project(new Base("Teachers"), attr);
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+
 //	@Test
 //	void testProjectSets() throws SchemaException {
 //		List<String> attr = new ArrayList<>();
@@ -215,19 +197,15 @@ class OrderedTest {
 //		expected.add(Record.valueOf(types, "s6"));
 //		assertEquals(expected, e.execute(dbSets));
 //	}
-//
-//	@Test
-//	void testDifferenceBags() throws SchemaException {
-//		// SportStudents <D> Students
-//		RAExpr e = new Difference(new Base("SportStudents"), new Base("Students"));
-//		Signature sig = dbBags.getSchema().getSignature("SportStudents");
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(sig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Sarah", "15", "s4"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Sean", "14", "s3"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//
+
+	@Test
+	void testDifferenceBags() throws SchemaException, IOException {
+		// SportStudents <D> Students
+		RAExpr e = new Difference(new Base("SportStudents"), new Base("Students"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+
 //	@Test
 //	void testDifferenceSets() throws SchemaException {
 //		// SportStudents <D> Students
@@ -238,17 +216,15 @@ class OrderedTest {
 //		expected.add(Record.valueOf(sig.getTypes(), "Sean", "14", "s3"));
 //		assertEquals(expected, e.execute(dbSets));
 //	}
-//	
-//	@Test
-//	void testIntersectBags() throws SchemaException {
-//		// Students <I> SportStudents
-//		RAExpr e = new Intersect(new Base("Students"), new Base("SportStudents"));
-//		Signature sig = dbBags.getSchema().getSignature("Students");
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(sig.getTypes(), "Hamish", "18", "s6"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//	
+	
+	@Test
+	void testIntersectBags() throws SchemaException, IOException {
+		// Students <I> SportStudents
+		RAExpr e = new Intersect(new Base("Students"), new Base("SportStudents"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+	
 //	@Test
 //	void testIntersectSets() throws SchemaException {
 //		// DetentionStudents <I> SportStudents
@@ -257,19 +233,15 @@ class OrderedTest {
 //		Table expected = new Table(sig);
 //		assertEquals(expected, e.execute(dbSets));
 //	}
-//	
-//	@Test
-//	void testProductBags() throws SchemaException {
-//		// Parents <X> City
-//		RAExpr e = new Product(new Base("Parents"), new Base("City"));
-//		Signature sig = Utils.concat(dbBags.getSchema().getSignature("Parents"), dbBags.getSchema().getSignature("City"));
-//		Table expected = new Table(sig);
-//		expected.add(Record.valueOf(sig.getTypes(), "Hugo", "3/09/88", "2", "Edinburgh"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Hugo", "3/09/88", "2", "London"));
-//		expected.add(Record.valueOf(sig.getTypes(), "Hugo", "3/09/88", "2", "Glasgow"));
-//		assertEquals(expected, e.execute(dbBags));
-//	}
-//	
+	
+	@Test
+	void testProductBags() throws SchemaException, IOException {
+		// Parents <X> City
+		RAExpr e = new Product(new Base("Parents"), new Base("City"));
+		Table expected = getExpectedTable(e.toString());
+		assertEquals(expected, e.execute(dbBags));
+	}
+	
 //	@Test
 //	void testProductSets() throws SchemaException {
 //		// Parents <X> City
